e2f96614ed3f0b5e92b48fd2b12bedbdb13e6925
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-07-24 13:58:09 -0300

    fix: change root directory logic

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,1 @@
-import path from 'path';
-
-import { environment } from './environment';
-
-export const ROOT_DIRECTORY =
-  environment.NODE_ENV === 'production' ? path.join(__dirname, '..') : path.join(__dirname, '..', '..');
+export const ROOT_DIRECTORY = process.cwd();
