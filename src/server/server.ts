b9587b9358d07ff7d86794f098f2333528424aee
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-07-27 16:52:18 -0300

    perf: improve cache strategy

--- src/server/server.ts
+++ src/server/server.ts
@@ -11,14 +11,16 @@ import { City } from '../types/locations';
 import { calculateDistanceByCoordinates } from '../utils/distances';
 import { handleServerError, NotFoundError } from './errors';
 
-const DEFAULT_CACHE_CONTROL_MAX_AGE = 60 * 60 * 24; // 1 day
+const DEFAULT_CACHE_CONTROL_LOCAL_MAX_AGE = 60 * 60; // 1 hour
+const DEFAULT_CACHE_CONTROL_REMOTE_MAX_AGE = 60 * 60 * 24 * 7; // 7 days
 const DEFAULT_CACHE_CONTROL_STALE_WHILE_REVALIDATE = 60; // 1 minute
 
 const DEFAULT_PUBLIC_CACHE_CONTROL_HEADER =
   environment.NODE_ENV === 'production'
     ? [
         'public',
-        `s-maxage=${DEFAULT_CACHE_CONTROL_MAX_AGE}`,
+        `max-age=${DEFAULT_CACHE_CONTROL_LOCAL_MAX_AGE}`,
+        `s-maxage=${DEFAULT_CACHE_CONTROL_REMOTE_MAX_AGE}`,
         `stale-while-revalidate=${DEFAULT_CACHE_CONTROL_STALE_WHILE_REVALIDATE}`,
       ].join(', ')
     : undefined;
